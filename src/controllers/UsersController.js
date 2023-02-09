e704200d07a10789bee51c6aa9ea5d612d9b14a0
Edna <edna.maria887@gmail.com>
2023-02-09 06:13:45 -0300

    adding user registration

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,22 +1,26 @@
-const AppError = require('../utils/AppError')
-const sqliteConnection = require('../database/sqlite')
+const AppError = require('../utils/AppError');
+const sqliteConnection = require('../database/sqlite'); //conexão com o banco de dados
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    const database = await sqliteConnection()
+    const database = await sqliteConnection(); //fazendo uma conexão com o banco de dados
 
-    const checkUserExist = await database.get(
+    const checkUserExist = await database.get( //consultando os dados da minha tabela
       'SELECT * FROM users WHERE email = (?)',
       [email]
-    )
+    );
 
-    if (checkUserExist) {
-      throw new AppError('Este e-mail já está em uso!')
+    if(checkUserExist) {
+      throw new AppError('Este e-mail já está em uso!');
     }
 
-    return response.status(201).json()
+    // INSERINDO DADOS DO USUÁRIO
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+    [name, email,password]);
+
+    return response.status(201).json();
   }
 }
 

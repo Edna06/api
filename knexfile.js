87bd7c5f8e85557baab23d67695c0e476d1ab4f2
Edna <edna.maria887@gmail.com>
2023-02-22 20:31:59 -0300

    addiction tables links and tags

--- knexfile.js
+++ knexfile.js
@@ -7,12 +7,22 @@ module.exports = {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
     // para automatizar a criação de tabelas dentro da minha aplicação
-    // em que lugar vasi ter que armazenar essas informações
-    migrations: { //o diretório deve conter o mesmo nome da pasta local da migration
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+
+    //para que eu consiga utilizar a opção de apagar por CASCADE
+    //recuperando minha conexão e a função de callback
+    pool: {
+      afterCreate: (conn, cb ) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      )
     },
     useNullAsDefault: true
   }
 }
-
-

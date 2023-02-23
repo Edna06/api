f823f240ee19d7be33869486898be5243fe9057b
Edna <edna.maria887@gmail.com>
2023-02-23 15:51:07 -0300

    registering notes

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,12 @@
+// responsabilidade de conter as rotas das notas
+
+const { Router } = require('express')
+const NotesController = require('../controllers/notesController')
+
+const notesRoutes = Router()
+
+const notesController = new NotesController()
+
+notesRoutes.post('/:user_id', notesController.create) 
+
+module.exports = notesRoutes

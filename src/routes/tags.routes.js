d53374190c4428d3ec450079f273de5b4672164c
Edna <edna.maria887@gmail.com>
2023-03-01 17:25:57 -0300

    listing the tags

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,13 @@
+// responsabilidade de conter as rotas do usuário
+
+const { Router } = require('express')
+const TagsController = require('../controllers/tagsController')
+
+const tagsRoutes = Router()
+
+const tagsController = new TagsController()
+
+
+tagsRoutes.get("/:user_id", tagsController.index)
+
+module.exports = tagsRoutes

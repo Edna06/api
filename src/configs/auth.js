40e48071b580adf8fdb8deb473ea4ab6dd255733
Edna <edna.maria887@gmail.com>
2023-03-27 12:52:25 -0300

    added authentication settings

---
+++ src/configs/auth.js
@@ -0,0 +1,9 @@
+//deixaremos aqui as configurações de autenticação da nossa aplicação 
+
+module.exports = {
+  jwt: {
+    secret: "default",  
+    expiresIn: '1d'  
+  }
+
+}
--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -31,7 +31,7 @@ class notesController {
 
     await knex('tags').insert(tagsInsert)
 
-    response.json()
+    return response.json()
   }
 
   async show(request, response) {

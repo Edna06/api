474faab1f75e718ae42c687f0cfdeb9b4f97fb92
Edna <edna.maria887@gmail.com>
2023-03-30 18:23:26 -0300

    added  avatar destination| file avatar in database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,11 @@
 // responsabilidade de conter as rotas do usuário
 
 const { Router } = require('express')
-const UsersController = require('../controllers/UsersController')
 const multer = require('multer')
 const uploadConfig = require('../configs/upload')
+
+const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
 
 // pegando o middle de autenticação
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
@@ -12,18 +14,9 @@ const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update) //put -> atualiza vários campos
-usersRoutes.patch(
-  '/avatar',
-  ensureAuthenticated,
-  upload.single('avatar'),
-  (request, response) => {
-    console.log(request.file.filename)
-
-    response.json()
-  }
-)
-
+usersRoutes.patch( '/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update) 
 module.exports = usersRoutes

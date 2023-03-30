a7954070603564a1228ad35ecedbf77debb84dc6
Edna <edna.maria887@gmail.com>
2023-03-29 23:25:13 -0300

    added avatar and upload img

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,29 +1,27 @@
-const fs = require('fs') // é do próprio node, e nos ajuda a lidar com manipulação de arquivos
+const fs = require('fs')
 const path = require('path')
 const uploadConfig = require('../configs/upload')
 
 class DiskStorage {
   async saveFile(file) {
     await fs.promises.rename(
-      //rename nãe é mudar o nome do arquivo não, estamos nos referindo a mudar de lugar
-      path.resolve(uploadConfig.TMP_FOLDER, file), //tira o file da pasta temporária
-      path.resolve(uploadConfig.UPLOADS_FOLDER, file) // adiciona ela na pasta de upload (que é onde, de fato, ela irá ficar)
-    ) // quando fizermos upload de uma imagem no backend, ela irá ficar esperando na pasta temporária - ela ficará esperando na pasta temporária para o nosso backend decidir o que fará com a imagem.
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    )
 
     return file
   }
 
   async deleteFile(file) {
-    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file) //buscando pelo arquivo na pasta uploads
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
 
-    //tratamento de exceções (caso o arquivo não exista mais)
-    try{
-      await fs.promises.static(filePath)
+    try {
+      await fs.promises.stat(filePath)
     } catch {
-      return //caso alguma coisa dê errado, ele irá retornar 
+      return
     }
 
-    await fs.promises.unlink(filePath) //vai deletar através do unlink
+    await fs.promises.unlink(filePath)
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,15 +2,28 @@
 
 const { Router } = require('express')
 const UsersController = require('../controllers/UsersController')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
-// pegando o middle de autenticação 
+// pegando o middle de autenticação
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
 
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create) 
-usersRoutes.put("/", ensureAuthenticated, usersController.update) 
+usersRoutes.post('/', usersController.create)
+usersRoutes.put('/', ensureAuthenticated, usersController.update) //put -> atualiza vários campos
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file.filename)
+
+    response.json()
+  }
+)
 
 module.exports = usersRoutes

c33e0e74dabd9c98a9effe751b9173ad99e63241
Edna <edna.maria887@gmail.com>
2023-07-20 12:29:37 -0300

    added describe and config email exists

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,21 +1,59 @@
 //Para cada serviço teremos um teste
-const UserCreateService = require("./UserCreateService");
-const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const UserCreateService = require('./UserCreateService')
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
+const AppError = require('../utils/AppError');
 
-//garante que o usuário seja criado
-it("user should be create", async () => { //adicionei o async aqui para poder utilizar o await
-  //dados fictícios para fazer a simulação dos dados que foram enviados por uma requisição
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  }; // simularei os inputs (valores enviados)
+describe('UserCreateService', () => { //testar coisas que estão dentro dessa regra de nogócio
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+  let userRepositoryInMemory = null;
+  let userCreateService = null;
 
-  console.log(userCreated)
+ beforeEach(() => {
+  userRepositoryInMemory = new UserRepositoryInMemory();
+  userCreateService = new UserCreateService(userRepositoryInMemory)
+ });
 
-  expect(userCreated).toHaveProperty("id");
+
+
+  //garante que o usuário seja criado
+  it('user should be create', async () => {
+    //adicionei o async aqui para poder utilizar o await
+    //dados fictícios para fazer a simulação dos dados que foram enviados por uma requisição
+    const user = {
+      name: 'User Test',
+      email: 'user@test.com',
+      password: '123'
+    } // simularei os inputs (valores enviados)
+
+
+    const userCreated = await userCreateService.execute(user)
+
+    console.log(userCreated)
+
+    expect(userCreated).toHaveProperty('id')
+  });
+
+
+
+  it("user not should be create with exists email", async () => {
+    const user1 = {
+      name: "User test 1",
+      email: "user@test.com",
+      password: "123"
+    };
+
+    const user2 = {
+      name: "User test 2",
+      email: "user@test.com",
+      password: "456"
+    };
+
+    await userCreateService.execute(user1);
+
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso!"));
+  });
 });
+
+
+
+
